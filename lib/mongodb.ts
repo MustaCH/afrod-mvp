3af8ba9e0bf769fa8335f508bfd2571372790cf5
ignacio poletti <ip@discoverit.store>
2025-02-18 15:50:48 -0300

    AFIT-001 - Creado entorno de NextJS, integrado MongoDB

---
+++ lib/mongodb.ts
@@ -0,0 +1,26 @@
+import { MongoClient, MongoClientOptions } from 'mongodb';
+
+const uri = process.env.MONGODB_URI as string;
+const options: MongoClientOptions = {};
+
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Please add your MongoDB URI to .env.local');
+}
+
+if (process.env.NODE_ENV === 'development') {
+  // En desarrollo, usa una variable global para evitar múltiples conexiones.
+  if (!(global as any)._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    (global as any)._mongoClientPromise = client.connect();
+  }
+  clientPromise = (global as any)._mongoClientPromise;
+} else {
+  // En producción, crea una nueva conexión.
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
