22fe67c32209357d84b679da1fe5d3dfafd36869
Ignacio Nicolás García Poletti <polettiignacio7@gmail.com>
2025-02-20 14:15:26 -0300

    FEATURE | Added Admin type

--- types/index.ts
+++ types/index.ts
@@ -1,35 +1,50 @@
 //User
 
-
 export type IUser = {
-    name: string,
-    lastname: string,
-    username: string,
-    email: string,
-    password: string,
-    subscription?: ISubscriptionType
-}
+  name: string;
+  lastname: string;
+  username: string;
+  email: string;
+  password: string;
+  subscription?: ISubscriptionType;
+};
 
 //Subscription
 
 export type ISubscriptionType = {
-    enabled: boolean,
-    tier: ISubscriptionTier
-}
+  enabled: boolean;
+  tier: ISubscriptionTier;
+};
 
 export enum ISubscriptionTier {
-    NONE = 'NONE',
-    BASIC = 'BASIC',
-    SILVER = 'SILVER',
-    GOLD = 'GOLD',
-  }
+  NONE = "NONE",
+  BASIC = "BASIC",
+  SILVER = "SILVER",
+  GOLD = "GOLD",
+}
 
 //Video
 
 export type IVideo = {
-    title: string,
-    description?: string,
-    src: string,
-    date: Date,
-    score?: number,
+  title: string;
+  description?: string;
+  src: string;
+  date: Date;
+  score?: number;
+};
+
+//AdminUser
+
+export type IAdminUser = {
+  name: string;
+  lastname: string;
+  username: string;
+  email: string;
+  password: string;
+  role: IAdminRole;
+};
+
+export enum IAdminRole {
+  ADMIN = "ADMIN",
+  SUPERADMIN = "SUPERADMIN",
 }
