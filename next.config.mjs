fb941a5e93449695546d675912949b1ae1485019
Ignacio Nicolás García Poletti <polettiignacio7@gmail.com>
2025-03-11 14:39:01 -0300

    FEATURE | Increase file upload limits and configure API function settings for video processing

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  api: {
-    bodyParser: {
-      sizeLimit: "50mb", // Aumenta según necesites (ej: 100mb, 200mb)
-    },
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
